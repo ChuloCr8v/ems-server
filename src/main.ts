38276d47078e5c591f88207af74b891c2de5c864
Victor Ogunwehin <victorogunwehin@gmail.com>
2025-08-12 11:24:14 +0100

    swagger_live

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:5173', // frontend URL
+    credentials: true,               // allow cookies if needed
+  });
 
   // Global validation pipe
   app.useGlobalPipes(new ValidationPipe());
