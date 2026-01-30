df1739f5edcd552c7d4f662b522941283cc820b1
Nkematu Bonaventure <74823094+ChuloCr8v@users.noreply.github.com>
2026-01-30 16:19:27 +0100

    pptr path update

--- src/puppeteer/puppeteer.service.ts
+++ src/puppeteer/puppeteer.service.ts
@@ -30,7 +30,6 @@ export class PuppeteerService implements OnModuleDestroy {
       this.browser = null;
     });
   }
-
   async getBrowser(headless = true): Promise<Browser> {
     if (this.browser && this.browser.connected) {
       return this.browser;
@@ -40,8 +39,6 @@ export class PuppeteerService implements OnModuleDestroy {
 
     this.browser = await puppeteer.launch({
       headless,
-      // dev only
-      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || "/usr/bin/chromium",
       args: [
         '--no-sandbox',
         '--disable-setuid-sandbox',
