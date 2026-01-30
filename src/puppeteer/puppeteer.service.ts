a21d3489bb3c35358f4f45676798c5efb00f3a93
Nkematu Bonaventure <74823094+ChuloCr8v@users.noreply.github.com>
2026-01-30 02:15:24 +0100

    fix: pptr

--- src/puppeteer/puppeteer.service.ts
+++ src/puppeteer/puppeteer.service.ts
@@ -41,7 +41,7 @@ export class PuppeteerService implements OnModuleDestroy {
     this.browser = await puppeteer.launch({
       headless,
       // dev only
-      executablePath: "/usr/bin/google-chrome-stable",
+      // executablePath: "/usr/bin/google-chrome-stable",
       args: [
         '--no-sandbox',
         '--disable-setuid-sandbox',
