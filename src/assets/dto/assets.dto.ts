6685319278e9d9fe72f20657b3431f1a69e2c009
omnisteph <omnisteph2000@gmail.com>
2026-04-11 18:24:01 +0100

    return asset2

--- src/assets/dto/assets.dto.ts
+++ src/assets/dto/assets.dto.ts
@@ -68,11 +68,11 @@ export class CreateAssetDto {
   @IsString()
   serialNo?: string;
 
-  // @ApiProperty({
-  //   enum: AssetCategory,
-  //   description: 'Category of the asset',
-  //   example: 'IT_EQUIPMENT',
-  // })
+  @ApiProperty({
+    enum: AssetCategory,
+    description: 'Category of the asset',
+    example: 'IT_EQUIPMENT',
+  })
   @IsNotEmpty()
   @IsString()
   category: AssetCategory;
